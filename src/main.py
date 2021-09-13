bfa35c8ab04fb97bcc23f910e430334743e8b4cf
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2021-09-13 20:39:08 +0900

    example voice channel bot

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import discord
 import os
 
-TOKEN=os.getenv("PUBLIC_KEY")
+TOKEN=os.getenv("TOKEN")
 
 client = discord.Client()
 
@@ -14,7 +14,19 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot:
         return
-    if message.content == "/example":
-        await message.channel.send("Succeeded")
+
+    if message.content == "!join":
+        if message.author.voice is None:
+            await message.channel.send("You aren't currently connected to a Voice Channel.")
+            return
+        await message.author.voice.channel.connect()
+        await message.channel.send("Connected.")
+
+    if message.content == "!leave":
+        if message.guild.voice_client is None:
+            await message.channel.send("Unconneted.")
+            return
+        await message.guild.voice_client.disconnect()
+        await message.channel.send("Disconnected.")
 
 client.run(TOKEN)
