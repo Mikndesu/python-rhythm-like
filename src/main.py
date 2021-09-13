6f1ffbf37866818733fad23ad1a9f59038f71778
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2021-09-13 20:55:02 +0900

    add play god knows code

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 import discord
+import pytube
 import os
 
 TOKEN=os.getenv("TOKEN")
 
+test_url="https://www.youtube.com/watch?v=WWB01IuMvzA"
+
 client = discord.Client()
+
+def youtube_dl(url):
+    youtube = pytube.YouTube(url).streams.first().download("aaa.mp4")
+
 
 @client.event
 async def on_ready():
@@ -21,6 +28,10 @@ async def on_message(message):
             return
         await message.author.voice.channel.connect()
         await message.channel.send("Connected.")
+        voice_ch = await client.join_voice_channel(client.get_channel("Discord voice channel ID"))
+        youtube_dl(test_url)
+        player = voice_ch.create_ffmpeg_player("aaa.mp3")
+        player.start()
 
     if message.content == "!leave":
         if message.guild.voice_client is None:
