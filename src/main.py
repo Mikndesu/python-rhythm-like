49cfcb04f5bf8d87892851897c2f3a1d52a32be5
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2021-09-13 22:47:02 +0900

    add few changes

--- src/main.py
+++ src/main.py
@@ -44,10 +44,9 @@ async def on_message(message):
             await message.channel.send("You aren't currently connected to a Voice Channel.")
             return
         title_and_id = search(message.content.replace("!join ", ""))
-        await message.channel.send(f"Currently Playing {title_and_id[0]}")
+        await message.channel.send(f"Currently Playing: {title_and_id[0]}")
         youtube_dl(f"https://www.youtube.com/watch?v={title_and_id[1]}")
         await message.author.voice.channel.connect()
-        await message.channel.send("Connected.")
         message.author.guild.voice_client.play(discord.FFmpegPCMAudio('/home/ubuntu/python-rhythm-like/aaa.webm'), after=lambda e: print('done', e))
 
     if message.content == "!leave":
